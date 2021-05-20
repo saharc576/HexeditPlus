9b2f81f78ce40b3a96ab9376f4d7a0b2f4733e9c
sahar <sahar576cohen@gmail.com>
2021-05-20 13:14:03 +0300

    task 0 done

---
+++ task0b/hexeditplus.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "hexeditplus.h"
+
+
+int main(int argc, char **argv)
+{
+    struct fun_desc menu[] = {
+        {"Toggle Debug Mode", toggleDebugMode},
+        {"Set File Name", setFileName},
+        {"Set Unit Size", setUnitSize},
+        {"Load Into Memory", loadIntoMemory},
+        {"Memory Display", memoryDisplay},
+        {"Save Into File", saveIntoFile},
+        {"Memory Modify", memoryModify},
+        {"Quit", quit}
+        };
+
+    int upperBound = (sizeof(menu) / sizeof(struct fun_desc));
+    int i;
+    char input[BUF_SZ];
+    int choise;
+    char c;
+    state *s = (state*)calloc(sizeof(state), 1);
+
+    while (1) 
+    {
+        printf("Choose actoin:\n");
+
+        for (i = 0; i < upperBound ; i++) 
+        {
+            printf("%d- %s\n", i, menu[i].name);
+        }
+
+        printf("option: ");
+        
+        
+        fgets(input,BUF_SZ,stdin);
+        sscanf(input, "%d%c", &choise, &c);
+
+        if ( c != '\n' || choise >= upperBound || choise < 0) 
+        {
+            printf("Not within bounds\n");
+            // free(carray);
+            exit(EXIT_FAILURE);
+        }
+        else 
+        {
+            printf("Within bounds\n");
+        }
+        
+        menu[choise].fun(s);
+
+        printf("DONE.\n");
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+void toggleDebugMode (state *s)
+{
+    if (s->debug_mode == 0)
+    {
+        s->debug_mode = 1;
+        printf("Debug flag now on\n");
+    }
+    else if (s->debug_mode == 1)
+    {
+        s->debug_mode = 0;
+        printf("Debug flag now off\n");
+    }
+
+}
+
+void setFileName (state *s)
+{
+    char input[NAME_LEN];
+    char file[NAME_LEN];
+    printf("Enter file name: ");
+    fflush(stdout);
+     
+    fgets(input,BUF_SZ,stdin);
+    sscanf(input, "%s", file);
+
+    strcpy(s->file_name, file);
+
+    if (s->debug_mode == 1)
+    {
+        fprintf(stderr, "Debug: file name set to %s\n", s->file_name);
+        fflush(stderr);
+    }
+
+}
+void setUnitSize (state *s)
+{
+    char input[BUF_SZ];
+    int choise;
+    char c;
+    printf("Enter number: ");
+    fflush(stdout);  
+
+    fgets(input,BUF_SZ,stdin);
+    sscanf(input, "%d%c", &choise, &c);
+
+    if ( c != '\n' || (choise != 1 && choise != 2 && choise != 4)) 
+    {
+        printf("Not a valid size\n");
+        return;
+    }
+
+    s->unit_size = choise;
+
+    if (s->debug_mode == 1)
+    {
+        fprintf(stderr, "Debug: set size to %d\n", s->unit_size);
+        fflush(stderr);
+    }
+}
+void loadIntroMemory (state *s) 
+{
+
+
+}
+void loadIntoMemory (state *s)
+{
+
+}
+void memoryDisplay (state *s)
+{
+
+}
+void saveIntoFile (state *s)
+{
+
+}
+void memoryModify (state *s)
+{
+
+}
+void quit (state *s)
+{
+    if (s->debug_mode == 1)
+    {
+        fprintf(stderr, "Debug: quitting\n");
+        fflush(stderr);
+    }
+    exit(EXIT_SUCCESS);
+}
---
+++ task0b/hexeditplus.h
@@ -0,0 +1,36 @@
+#define  NAME_LEN  128
+#define  BUF_SZ    10000
+
+typedef struct {
+  char debug_mode;
+  char file_name[NAME_LEN];
+  int unit_size;
+  unsigned char mem_buf[BUF_SZ];
+  size_t mem_count;
+  /*
+   .
+   .
+   Any additional fields you deem necessary
+  */
+} state;  
+  
+  struct fun_desc {
+        char *name;
+        void (*fun)(state*);
+    };
+
+
+
+/* When the debug mode is on, the following values in the state are printed:
+   unit_size, file_name, and mem_count,
+   every time just before the menu is printed*/     
+void toggleDebugMode (state*);
+
+void setFileName (state *s);
+void setUnitSize (state *s);
+void loadIntroMemory (state *s);
+void loadIntoMemory (state *s);
+void memoryDisplay (state *s);
+void saveIntoFile (state *s);
+void memoryModify (state *s);
+void quit (state *s);
